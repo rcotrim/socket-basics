c28e6717ce1939b05a483108bb959d5482b65a44
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-08-11 15:52:33 -0300

    Added custom messagem event

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 var socket = io();
+socket.on('connect', function () {
+    console.log('Conectado ap servidor socket.io!');
+});
+
+socket.on('mensagem', function (message) {
+    console.log('nova mensagem:');
+    console.log(message.text);
+})
--- server.js
+++ server.js
@@ -6,10 +6,20 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connect', function () {
+io.on('connect', function (socket) {
     console.log('User conectado via socket.io');
-});
 
+    socket.on('mensagem', function (message) {
+        console.log('Mensagem recebida: ' + message.text);
+
+        //io.emit - manda para todos inclusive o que enviou
+        socket.broadcast.emit('mensagem', message)       //manda para todos menos o que mandou
+    })
+
+    socket.emit('mensagem', {
+        text: 'Bem vindo à aplicação de Chat'
+    });
+});
 
 http.listen(PORT, function() {
     console.log('Servidor no ar !!');
