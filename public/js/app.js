2dc41b37d3376def29471011ca09e12ea74e6c94
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-08-13 15:07:48 -0300

    Add Bootstrap on chat page

--- public/js/app.js
+++ public/js/app.js
@@ -16,11 +16,16 @@ socket.on('connect', function () {
 
 socket.on('mensagem', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);
-    var $message = jQuery('.messages');
+    var $messages = jQuery('.messages');
+    var $message = jQuery('<li class="list-group-item"></li>')
     console.log('nova mensagem:');
     console.log(message.text);
-    $message.append('<p><strong>' + message.name + " - " + momentTimestamp.local().format('H:mm') + '</strong>' + '</p>');
+    $message.append('<span class="badge">' + momentTimestamp.local().format('H:mm') + '</span>');
+    //$message.append('<p><strong>' + message.name + " - " + momentTimestamp.local().format('H:mm') + '</strong>' + '</p>');
+    //$message.append('<p><strong>' + message.name + '</strong>' + '</p>');
+    $message.append('<h4 class = "list-group-item-heading">' + message.name + '</h4>');
     $message.append('<p>'+ message.text + '</p>');
+    $messages.append($message);
 });
 
 // Handles submitting of new message
